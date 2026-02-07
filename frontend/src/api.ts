b178ac1423aa453c1c637f3841bd62d2a23850f3
varun.singh <varunpratap74358@gmail.com>
2026-02-07 13:04:56 +0530

    Backend hosted

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,2 +1,3 @@
-const BACKEND_API:String = "http://localhost:8000";
+// const BACKEND_API:String = "http://localhost:8000";
+const BACKEND_API:String = "https://jd-llm-rag-system-1.onrender.com";
 export default BACKEND_API
